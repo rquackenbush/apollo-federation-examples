7359eebebcc4ddd416255b83f6765f49775c26e1
Rich Quackenbush <Richard.Quackenbush@AcuityBrands.com>
2024-06-19 18:03:24 -0400

    Initial

---
+++ src/ApolloFederationRepro/Program.cs
@@ -0,0 +1,27 @@
+﻿using ApolloFederationRepro.Types;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddApolloFederationV2();
+
+builder
+   .Services
+   .AddCors(options =>
+   {
+       options.AddDefaultPolicy(builder =>
+       {
+           builder
+               .WithOrigins("https://studio.apollographql.com")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+       });
+   });
+
+var app = builder.Build();
+
+app.UseCors();
+
+app.MapGraphQL();
+
+app.Run();
---
+++ src/ApolloFederationRepro/Types/Widget.cs
@@ -0,0 +1,27 @@
+﻿using ApolloGraphQL.HotChocolate.Federation;
+
+namespace ApolloFederationRepro.Types
+{
+    [Key("id")]
+    [GraphQLDescription("A thing.")]
+    public class Widget
+    {
+        public Widget(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        [GraphQLDescription("Id of the widget")]
+        [ID]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        [ReferenceResolver]
+        public static Widget Get(string id)
+        {
+            return new Widget("42", "Widget from Get");
+        }
+    }
+}
